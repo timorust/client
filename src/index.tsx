b8cd4a0d27a7e6f1e9e7dace7461ac1ef4684c7f
tamir.rust <timorust1@gmail.com>
2024-01-08 13:57:40 +0200

    add type

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import { Paths } from "./paths";
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>,
+  },
+  {
+    path: Paths.register,
+    element: <h1>Register</h1>,
+  },
+]);
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,12 +28,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello World</h1>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+  home: "/",
+  addConsumer: "/consumer/add",
+  editConsumer: "/consumer/edit",
+  consumer: "/consumer",
+  status: "/status",
+  login: "/login",
+  register: "/register",
+} as const;
